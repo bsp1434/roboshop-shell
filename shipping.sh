df2b115d4b1e98596b117c9c052ede6fc9e7bd5c
Sivaprasad <sivaprasad.b1434@gmail.com>
2023-04-19 20:01:12 +0530

    cart application created

--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,15 @@
 yum install maven -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
 unzip /tmp/shipping.zip
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
-cp shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
 systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
 yum install mysql -y
 mysql -h mysql.devopspractice72.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+systemctl enable shipping
 systemctl restart shipping
