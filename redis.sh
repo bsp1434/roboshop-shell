3494ff2bc7deefe23c60a3d9cc6ef1ad158b29ce
Sivaprasad <sivaprasad.b1434@gmail.com>
2023-04-19 19:36:59 +0530

    redis update with port

--- redis.sh
+++ redis.sh
@@ -4,8 +4,8 @@ yum install redis -y
 
 ##edit the redis.conf file and replace the port 127.0.0.1 to 0.0.0.0
 
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
