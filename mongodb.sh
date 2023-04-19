d6b3f8bf1c9409d408ae48992051ee1a39625759
Sivaprasad <sivaprasad.b1434@gmail.com>
2023-04-19 18:12:57 +0530

    created frontend script and config file

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,9 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
 
 #edit the mongod.repo file and replace the port 127.0.0.1 to 0.0.0.0
+
+
